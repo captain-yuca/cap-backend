f9a4ab4d071397733381c2699923dc762bea1181
Manuel Baez <manuel.baez@upr.edu>
2018-12-05 07:23:54 -0500

    Added s

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ import io from 'socket.io'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import { LevelEntrySchema } from './src/models/levelEntryModel'
 
 const SerialPort = require('serialport'); 
 const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('/dev/ttyACM1')
+const port = new SerialPort('/dev/ttyACM0')
 const parser = port.pipe(new Readline({delimiter: '\r\n'}));
-const PORT = 3000;
+const PORT = 80;
 
 
 const app = express();
+mongoose.Promise= global.Promise
+mongoose.connect('mongodb://admin:capstone2018@ds117773.mlab.com:17773/fmds', {
+    useMongoClient: true
+})
 
 app.use(cors())
-
-
-
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json())
 routes(app)
 
 app.get('/', (req, res) => {
@@ -37,6 +41,12 @@ parser.on('data', (temp) => {
         sock.emit('message', {
             name: new Date().toISOString(),
             value: temp
+        })
+        let newEntry = new LevelEntry({level:temp})
+        newEntry.save((err, entry) => {
+            if(err){
+                throw err;
+            }
         })
     }
     catch(err){
